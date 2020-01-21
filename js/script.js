782de032c8ede841c03abc801d56c4e7a05409cf
Andrea Bottini <andrea.bottini22@gmail.com>
2020-01-21 15:22:07 +0100

    Inserimento Alert con 5 numeri

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
 
 // alert('funziona?')
+
+// Devo fare in modo che un alert crei 5 numeri casuali. Per fare ciò creo la funzione e utilizzo getRandomNumber
+
+var numeriAlert = [];
+for (var i = 0; i < 5; i++) {
+  numeriAlert.push(getRandomNumber(1, 100))
+}
+alert(numeriAlert);
+
+
+// FUNZIONI
+function getRandomNumber(numMin, numMax) {
+  numMin = Math.ceil(numMin);
+  numMax = Math.floor(numMax);
+  var risultato = Math.floor(Math.random() * (numMax - numMin + 1)) + numMin;
+  return risultato;
+}
